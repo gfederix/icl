c74f44337f504f7db42b6f6bdf67a6eac64c809a
Fyodor P. Goncharov <gfederix@gmail.com>
2017-09-04 14:09:40 +0700

    terminal read interface

--- src/terminal/terminal.hpp
+++ src/terminal/terminal.hpp
@@ -1,11 +1,15 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <string>
+#include <memory>
+
 class Terminal {
 private:
   termios oldTermios;
   termios rawTermios;
   winsize wsz;
+  using int_type = int;
+  using char_type = char;
 public:
   Terminal();
   ~Terminal();
@@ -14,6 +18,24 @@ public:
   }
   unsigned short cols() {
     return wsz.ws_row;
+  }
+  int get() {
+    return std::getc(stdin);
+  }
+  Terminal& putback( char ch) {
+    std::ungetc(ch, stdin);
+    return *this;
+  }
+  int_type peek(){
+    int_type ch = get();
+    putback(ch);
+    return ch;
+  }
+  Terminal& read(char_type* s, std::streamsize count) {
+    while (count) {
+      s[--count] = std::getc(stdin);
+    };
+    return *this;
   }
   void raw();
 };
